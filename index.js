93c68f5b9c3038949db908f5e53b1cce93602af3
jamiu654 <akoredelekan444@gmail.com>
2026-03-31 11:16:27 +0100

    index.html index.css index.js
    A professional website

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typing', {
+    strings: ["A Developer", "A FullStack Engineer", "A Problem Solver"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+  });
+
+  
+    
